dd0f0e989229ab10c6a8f9e6de217aead9a9cc0d
Reda AGAOUA <reda.agaoua@gmail.com>
2025-01-06 11:39:19 -0500

    generate embedding now truncate the table before inserting

--- 01_generate_embeddings.sh
+++ 01_generate_embeddings.sh
@@ -5,6 +5,8 @@ readonly role="postgres"
 readonly psql="psql -h localhost -U "$role" -d "$db""
 
 ${psql[@]} <<EOF
+TRUNCATE ticket_conversation_embeddings;
+
 INSERT INTO ticket_conversation_embeddings
 SELECT id, openai.vector(conversation)::vector FROM ticket_conversations;
 EOF
